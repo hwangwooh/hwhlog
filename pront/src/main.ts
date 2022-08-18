8e6946cc43dde367b86fae9219cde9c031f24458
hwh <zaq8077@gmail.com>
2022-08-18 20:32:32 +0900

    vue post_test

--- pront/src/main.ts
+++ pront/src/main.ts
@@ -3,12 +3,17 @@ import { createPinia } from "pinia";
 
 import App from "./App.vue";
 import router from "./router";
+import ElementUI from 'element-plus';
+import ElementPlus from 'element-plus';
 
+import "element-plus/dist/index.css"
 import "./assets/main.css";
+import "bootstrap/dist/css/bootstrap-utilities.css";
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
-
+app.use(ElementUI);
+app.use(ElementPlus);
 app.mount("#app");
---
+++ pront/src/views/WriteView.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import {ref} from "vue";
+import axios from "axios";
+
+const count = ref(0);
+const  title = ref("");
+const  content = ref("");
+const write = function () {
+axios.post("http://localhost:8080/posts",{
+  title : title.value,
+  content : content.value
+})
+
+}
+
+</script>
+
+<template>
+  <div>
+    <el-input v-model="title" placeholder="제목을 입력해주세요"/>
+  </div>
+
+  <div>
+    <div class="mt-2">
+      <el-input v-model="content" type="textarea" rows="20"/>
+    </div>
+    <div class="mt-2">
+      <el-button type="primary" @click = "write()"> 글 작성 완료</el-button>
+    </div>
+  </div>
+
+</template>
+
+<style>
+
+</style>
