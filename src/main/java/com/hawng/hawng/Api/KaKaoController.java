00376fcb66123d55aeb5b5e6f2f5b461c3bf3169
hwh <zaq8077@gmail.com>
2022-09-01 18:25:30 +0900

    카카오 api

---
+++ src/main/java/com/hawng/hawng/Api/KaKaoController.java
@@ -0,0 +1,27 @@
+package com.hawng.hawng.Api;
+
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.util.Map;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/oauth")
+public class KaKaoController {
+
+    @Autowired
+    private final KaKaoService kaKaoService;
+
+    @ResponseBody
+    @GetMapping("/kakao")
+    public void kakaoCallback(@RequestParam String code) {
+        System.out.println("kakaoCallback =  " +code);
+        String kakaoAccessToken = kaKaoService.getKakaoAccessToken(code);
+        System.out.printf("kakaoAccessToken   =  " + kakaoAccessToken);
+    }
+}
